5a09f1e5939b5e63f95d0595decd78b4bce8e248
sadamdamz <sadamdamzzz@gmail.com>
2021-02-12 10:25:40 +0530

    commit

--- App.js
+++ App.js
@@ -1,14 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+// import AppLoading from 'expo-app-loading';
+import { Button, View, StyleSheet } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
+const Drawer = createDrawerNavigator();
+
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        onPress={() => navigation.navigate('Notifications')}
+        title="Go to notifications"
+      />
     </View>
   );
+}
+
+function NotificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
+
+export default function App() {
+
+  const [isLoadingComplete, setIsLoadingComplete ] = useState(false);
+  const [fontLoaded, setFontLoaded] = useState(false)
+
+  const loadResourcesAsync = async () => {
+    await Font.loadAsync({
+      'montserrat-regular': require('./assets/font/Montserrat-Regular.ttf'),
+      'montserrat-bold': require('./assets/font/Montserrat-Bold.ttf')
+    });
+    setFontLoaded(true)
+    return
+    // return Promise.all([...cacheImages(assetImages)]);
+  };
+
+  const handleLoadingError = error => {
+    console.warn(error);
+  };
+
+  const handleFinishLoading = () => {
+    if (fontLoaded) {
+      setIsLoadingComplete(true);
+    }
+  };
+
+  return(
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
